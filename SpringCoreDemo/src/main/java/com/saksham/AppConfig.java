f2ffbebc72d3abc3b707426bc6a6683b9587337e
Saksham Katiyar <katiyar.saksham2004@gmail.com>
2026-06-29 00:58:02 +0530

    #5 Spring IoC Container

--- SpringCoreDemo/src/main/java/com/saksham/AppConfig.java
+++ SpringCoreDemo/src/main/java/com/saksham/AppConfig.java
@@ -1,6 +1,7 @@
 package com.saksham;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,4 +9,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.saksham")
 public class AppConfig {
 
+    @Bean
+    public User createUser() {
+        return new User("Saksham", 22);
+    }
 }
--- SpringCoreDemo/src/main/java/com/saksham/Main.java
+++ SpringCoreDemo/src/main/java/com/saksham/Main.java
@@ -8,11 +8,14 @@ public class Main {
     public static void main() {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        OrderService order = context.getBean(OrderService.class);//Uses Reflection
-        order.placeOrder();
+//        OrderService order = context.getBean(OrderService.class);//Uses Reflection
+//        order.placeOrder();
 
 //        PaymentService payment = context.getBean(PaymentService.class);
 //        payment.pay();
+
+        User u = context.getBean(User.class);
+        System.out.println(u.getName());;
     }
 }
 
